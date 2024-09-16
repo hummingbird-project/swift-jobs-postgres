352feeed19ac4c3259899ae34ad0d3e3d3569e48
Adam Fowler <adamfowler71@gmail.com>
2024-09-16 12:35:56 +0100

    Use hummingbird-postgres delete-jobs-postgres branch

--- Package.swift
+++ Package.swift
@@ -6,27 +6,26 @@ let package = Package(
     name: "swift-jobs-postgres",
     platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17)],
     products: [
-        .library(name: "JobsPostgres2", targets: ["JobsPostgres2"]),
+        .library(name: "JobsPostgres", targets: ["JobsPostgres"]),
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/swift-jobs.git", branch: "main"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-postgres.git", branch: "split-libraru"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-postgres.git", branch: "delete-jobs-postgres"),
         .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.21.0"),
     ],
     targets: [
         .target(
-            name: "JobsPostgres2",
+            name: "JobsPostgres",
             dependencies: [
                 .product(name: "PostgresMigrations", package: "hummingbird-postgres"),
                 .product(name: "Jobs", package: "swift-jobs"),
                 .product(name: "PostgresNIO", package: "postgres-nio"),
-            ],
-            path: "Sources/JobsPostgres"
+            ]
         ),
         .testTarget(
             name: "JobsPostgresTests",
             dependencies: [
-                "JobsPostgres2"
+                "JobsPostgres",
             ]
         ),
     ]
