9cade861fa09b595e50394fffc76c78022467d53
Adam Fowler <adamfowler71@gmail.com>
2025-02-21 17:41:13 +0000

    Decode job from Postgres JSON

---
+++ Sources/JobsPostgres/AnyDecodableJob+postgres.swift
@@ -0,0 +1,39 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2024-2025 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Jobs
+import PostgresNIO
+
+extension AnyDecodableJob: @retroactive PostgresDecodable {
+    public static var psqlType: PostgresDataType {
+        .bytea
+    }
+    public static var psqlFormat: PostgresFormat {
+        .binary
+    }
+
+    public init<JSONDecoder: PostgresJSONDecoder>(
+        from buffer: inout ByteBuffer,
+        type: PostgresDataType,
+        format: PostgresFormat,
+        context: PostgresDecodingContext<JSONDecoder>
+    ) throws {
+        switch (format, type) {
+        case (.binary, .bytea):
+            self = try context.jsonDecoder.decode(Self.self, from: buffer)
+        default:
+            throw PostgresDecodingError.Code.typeMismatch
+        }
+    }
+}
---
+++ Sources/JobsPostgres/JobRequest+postgres.swift
@@ -0,0 +1,33 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2024-2025 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Jobs
+import PostgresNIO
+
+extension JobRequest: @retroactive PostgresEncodable where Parameters: Encodable {
+    public static var psqlType: PostgresDataType {
+        .bytea
+    }
+    public static var psqlFormat: PostgresFormat {
+        .binary
+    }
+
+    @inlinable
+    public func encode<JSONEncoder: PostgresJSONEncoder>(
+        into byteBuffer: inout ByteBuffer,
+        context: PostgresEncodingContext<JSONEncoder>
+    ) throws {
+        try context.jsonEncoder.encode(self, into: &byteBuffer)
+    }
+}
