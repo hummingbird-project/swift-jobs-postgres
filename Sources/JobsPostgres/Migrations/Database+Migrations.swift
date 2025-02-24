34a68447ce4ee667c001d7c010fc47f5e4921c54
Stevenson Michel <130018170+thoven87@users.noreply.github.com>
2025-02-24 16:02:13 -0500

    adding migration group

---
+++ Sources/JobsPostgres/Migrations/Database+Migrations.swift
@@ -0,0 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2024 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import PostgresMigrations
+
+extension DatabaseMigrationGroup {
+    /// JobQueue migration group
+    public static var jobQueue: Self { .init("_hb_jobqueue") }
+}
