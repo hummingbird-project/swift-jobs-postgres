b0848756f8a8f5eef4123957584142dee791b0d2
Stevenson Michel <130018170+thoven87@users.noreply.github.com>
2025-03-13 11:11:49 -0400

    Used versioned released for hummingbird-postgres

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         // TODO: use a released version of swift-jobs
         .package(url: "https://github.com/hummingbird-project/swift-jobs.git", branch: "main"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-postgres.git", branch: "main"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-postgres.git", from: "0.6.0"),
         .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.25.0"),
     ],
     targets: [
