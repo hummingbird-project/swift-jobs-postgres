f9bace98f4efa22e27a27c779bf7743f5955f087
Adam Fowler <adamfowler71@gmail.com>
2026-07-29 10:48:10 +0100

    Add file header

--- Sources/JobsPostgres/UUID+v7.swift
+++ Sources/JobsPostgres/UUID+v7.swift
@@ -1,3 +1,22 @@
+//
+// This source file is part of the Hummingbird server framework project
+// Copyright (c) the Hummingbird authors
+//
+// See LICENSE.txt for license information
+// SPDX-License-Identifier: Apache-2.0
+//
+
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2022 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
 #if canImport(FoundationEssentials)
 import FoundationEssentials
 #else
