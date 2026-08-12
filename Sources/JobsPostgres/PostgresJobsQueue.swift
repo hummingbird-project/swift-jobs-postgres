a17663fc5626c9c9e444a06438943e5d0d54e338
Adam Fowler <adamfowler71@gmail.com>
2026-08-12 09:37:54 +0100

    Remove unused code

--- Sources/JobsPostgres/PostgresJobsQueue.swift
+++ Sources/JobsPostgres/PostgresJobsQueue.swift
@@ -280,11 +280,7 @@ public final class PostgresJobQueue: JobQueueDriver, CancellableJobQueue, Resuma
     /// This is called to say job has finished processing and it can be deleted
     @inlinable
     public func finished(jobID: JobID) async throws {
-        if configuration.retentionPolicy.completedJobs == .doNotRetain {
-            try await self.delete(jobID: jobID)
-        } else {
-            try await self.setStatus(jobID: jobID, status: .completed)
-        }
+        preconditionFailure("This should no longer be called. Use finished(jobID:retain:)")
     }
 
     /// This is called to say job has finished processing and it can be deleted
@@ -300,11 +296,7 @@ public final class PostgresJobQueue: JobQueueDriver, CancellableJobQueue, Resuma
     /// This is called to say job has failed to run and should be put aside
     @inlinable
     public func failed(jobID: JobID, error: any Error) async throws {
-        if configuration.retentionPolicy.failedJobs == .doNotRetain {
-            try await self.delete(jobID: jobID)
-        } else {
-            try await self.setStatus(jobID: jobID, status: .failed)
-        }
+        preconditionFailure("This should no longer be called. Use failed(jobID:error:retain:)")
     }
 
     /// This is called to say job has failed to run and should be put aside
